e57ef204495cb475ad958752789229a68b065657
pshreenath <prathima.shreenath@liferay.com>
2018-09-21 15:57:52 -0500

    Create HTTP Client Demo

---
+++ modules/http-client-demo/src/main/java/com/liferay/lsna/http/demo/constants/HttpClientDemoPortletKeys.java
@@ -0,0 +1,10 @@
+package com.liferay.lsna.http.demo.constants;
+
+/**
+ * @author Prathima Shreenath
+ */
+public class HttpClientDemoPortletKeys {
+
+	public static final String HTTP_CLIENT_DEMO = "http_client_demo";
+
+}
