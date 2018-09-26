2ea77a6de489c8bcceb9246d5db70f9b1ded669f
pshreenath <prathima.shreenath@liferay.com>
2018-09-26 12:15:10 -0500

    Add file to git

---
+++ modules/npm-angular-demo/src/main/java/com/liferay/lsna/npm/angular/demo/constants/NpmAngularDemoPortletKeys.java
@@ -0,0 +1,10 @@
+package com.liferay.lsna.npm.angular.demo.constants;
+
+/**
+ * @author Prathima Shreenath
+ */
+public class NpmAngularDemoPortletKeys {
+
+	public static final String NPM_ANGULAR_PORTLET = "npm_angular_portlet";
+
+}
