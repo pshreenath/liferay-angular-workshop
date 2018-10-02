52738c79931e60f8706796f5719ae1b35c5c9c9b
pshreenath <prathima.shreenath@liferay.com>
2018-10-02 15:09:33 -0500

    Create base for HTTP Client Demo

---
+++ modules/http-client-demo/README.md
@@ -0,0 +1,3 @@
+# http-client-demo
+
+HTTP Client Demo
---
+++ modules/http-client-demo/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+import LiferayParams from '../types/LiferayParams'
+
+@Component({
+	templateUrl: '/o/http-client-demo/app/app.component.html'
+})
+export class AppComponent {
+	params: LiferayParams;
+}
