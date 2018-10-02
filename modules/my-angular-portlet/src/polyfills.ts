513a849466828ab1dbcae2b63d26df5936c94a7f
pshreenath <prathima.shreenath@liferay.com>
2018-10-02 12:08:54 -0500

    Update portlet to use shared provider

--- modules/my-angular-portlet/src/polyfills.ts
+++ modules/my-angular-portlet/src/polyfills.ts
@@ -1,3 +1,4 @@
 // Import needed polyfills for the application
 import 'core-js/es7/reflect';
 import 'zone.js/dist/zone';
+import 'angular-npm-provider';
--- modules/provider/angular-npm-provider/src/index.js
+++ modules/provider/angular-npm-provider/src/index.js
@@ -9,3 +9,5 @@
  */
 export default function init() {
 }
+
+console.log('Bootstrapped angular-npm-provider');
