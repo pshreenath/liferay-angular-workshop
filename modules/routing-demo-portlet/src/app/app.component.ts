ba78a9da3c047341b1301d11e8eeb22afaa43fec
pshreenath <prathima.shreenath@liferay.com>
2018-10-02 14:01:16 -0500

    Create routing demo portlet

---
+++ modules/routing-demo-portlet/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+import LiferayParams from '../types/LiferayParams'
+
+@Component({
+	templateUrl: '/o/routing-demo-portlet/app/app.component.html'
+})
+export class AppComponent {
+	params: LiferayParams;
+}
