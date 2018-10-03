3c24dc115e902f0abd6480de3470fb14dcba2c91
pshreenath <prathima.shreenath@liferay.com>
2018-10-03 10:46:01 -0500

    Use Liferay object to call remote services

--- modules/http-client-demo/src/app/app.component.ts
+++ modules/http-client-demo/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import LiferayParams from '../types/LiferayParams'
 import {HttpClientService} from "../services/http-client.service";
 
+declare var Liferay: any;
 
 @Component({
 	templateUrl: '/o/http-client-demo/app/app.component.html'
@@ -10,16 +11,37 @@ import {HttpClientService} from "../services/http-client.service";
 export class AppComponent {
 	params: LiferayParams;
 	url: string = '';
+	authUrl: string = '';
 	json: string = '';
+	folderName: string = '';
+	groupId: number = 0;
 
 	constructor(
 			public httpClientService: HttpClientService) {
+
 	}
 
 	submit() {
-		this.httpClientService.getJson(this.url).subscribe((result) => {
+		this.authUrl = this.url + "?p_auth=" + Liferay.authToken;
+		this.httpClientService.getJson(this.authUrl).subscribe((result) => {
 			this.json = JSON.stringify(result);
 		})
 
+	}
+
+	addFolder() {
+		this.groupId = Liferay.ThemeDisplay.getScopeGroupId();
+		Liferay.Service(
+				'/dlapp/add-folder',
+				{
+					repositoryId: this.groupId,
+					parentFolderId: 0,
+					name: this.folderName,
+					description: this.folderName,
+				},
+				function(obj) {
+					console.log(obj);
+				}
+		);
 	}
 }
