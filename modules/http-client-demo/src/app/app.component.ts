a6f10b65de6adbdbdd964622f478a7d55c6413dc
pshreenath <prathima.shreenath@liferay.com>
2018-10-02 22:30:39 -0500

    Update existing file for http demo

--- modules/http-client-demo/src/app/app.component.ts
+++ modules/http-client-demo/src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
 
 import LiferayParams from '../types/LiferayParams'
+import {HttpClientService} from "../services/http-client.service";
+
 
 @Component({
 	templateUrl: '/o/http-client-demo/app/app.component.html'
 })
 export class AppComponent {
 	params: LiferayParams;
+	url: string = '';
+	json: string = '';
+
+	constructor(
+			public httpClientService: HttpClientService) {
+	}
+
+	submit() {
+		this.httpClientService.getJson(this.url).subscribe((result) => {
+			this.json = JSON.stringify(result);
+		})
+
+	}
 }
--- modules/http-client-demo/src/app/app.module.ts
+++ modules/http-client-demo/src/app/app.module.ts
@@ -1,18 +1,23 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {BrowserModule} from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {HttpClientService} from "../services/http-client.service";
 
 @NgModule({
-	imports: [BrowserModule],
+	imports: [
+		BrowserModule,
+		FormsModule,
+		HttpClientModule,
+		ReactiveFormsModule,
+	],
 	declarations: [AppComponent],
 	entryComponents: [AppComponent],
 	bootstrap: [], // Don't bootstrap any component statically (see ngDoBootstrap() below)
-	providers: [],
+	providers: [HttpClientService]
 })
 export class AppModule {
-	// Avoid bootstraping any component statically because we need to attach to
-	// the portlet's DOM, which is different for each portlet instance and,
-	// thus, cannot be determined until the page is rendered (during runtime).
 	ngDoBootstrap() {}
 }
